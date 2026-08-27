239000748937b6cb56f73742a2c62ee29f9016b0
Mike Hostetler <84222+mikehostetler@users.noreply.github.com>
2026-08-27 14:55:50 -0500

    refactor(pool): define adapter-neutral pool runtime boundary

--- lib/jido_browser/warm_pool/runtime.ex
+++ lib/jido_browser/warm_pool/runtime.ex
@@ -1,12 +1,21 @@
 defmodule Jido.Browser.WarmPool.Runtime do
   @moduledoc false
 
-  @callback start_worker(map()) :: {:ok, map()} | {:error, term()}
+  @type pool_state :: %{
+          required(:worker_opts) => keyword(),
+          optional(atom()) => term()
+        }
+
+  @callback start_worker(pool_state()) :: {:ok, map()} | {:error, term()}
   @callback command(map(), map(), pos_integer()) :: {:ok, map()} | {:error, term()}
   @callback shutdown_worker(map()) :: :ok | {:error, term()}
   @callback health_check(map()) :: :ok | {:error, term()}
 
   @optional_callbacks health_check: 1
+
+  @doc false
+  @spec context(pool_state()) :: term()
+  def context(pool_state), do: Map.get(pool_state, :runtime_context)
 
   @doc false
   @spec healthy?(module(), map()) :: :ok | {:error, term()}
